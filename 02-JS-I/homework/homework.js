ad4702e74583447123a8b54a11f8fb2fec900152
ramirinhojr <ramirojavierramos@gmail.com>
2022-11-14 19:12:45 -0300

    'tercer commit'

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -141,7 +141,10 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  return Math.floor(Math.random() * max);
+  console.log(getRandomInt(3));
+  console.log(getRandomInt(1));
+  console.log(Math.random());
 }
 
 function esPositivo(numero) {
